ea6cca07203ae5c2c9fe877bb7a27c4aff5a1b9f
3Douglas <3dpihl@gmail.com>
2020-09-22 11:00:06 -0500

    Update gen-manpages.sh

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,11 +7,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/litecoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/litecoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/zeatacoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/zeatacoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/zeatacoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/zeatacoin-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/zeatacoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
