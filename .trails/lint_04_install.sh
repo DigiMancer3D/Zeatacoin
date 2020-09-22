0121c40dd3dfc8f28525304e6c2b99794be219b4
3Douglas <3dpihl@gmail.com>
2020-09-22 10:18:44 -0500

    Update and rename install to lint_04_install.sh

--- .trails/lint_04_install.sh
+++ .trails/lint_04_install.sh
@@ -3,12 +3,13 @@
 # Copyright (c) 2018 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# copied from .travis
 
 export LC_ALL=C
 
-travis_retry pip install codespell==1.13.0
-travis_retry pip install flake8==3.5.0
-travis_retry pip install vulture==0.29
+trails_retry pip install codespell==1.13.0
+trails_retry pip install flake8==3.5.0
+trails_retry pip install vulture==0.29
 
 SHELLCHECK_VERSION=v0.6.0
 curl -s "https://storage.googleapis.com/shellcheck/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar --xz -xf - --directory /tmp/
